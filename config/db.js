7de3246e52dcbd118e20dccb124477c703a17cd7
vishakha <vishakha@gmail.com>
2025-06-09 08:41:22 +0530

    Clean install for render deployment

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    //const conn = await mongoose.connect(process.env.ATLASDB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB Connection Error:', err);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
